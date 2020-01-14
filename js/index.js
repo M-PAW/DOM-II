b25cf95978daccbbf8a94cea2630eb7cc0e23296
Michael Walter <Michael_Patrick.Walter@outlook.com>
2020-01-14 15:06:26 -0800

    Completed all 10 behaviors

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 // Your code goes here
 
+
+// 1 - 2. mouseenter / mouseleave
 const navLink = document.querySelectorAll('.nav-link').forEach(el => {
     el.addEventListener('mouseenter', () => {
         el.style.color = 'orange';
@@ -12,6 +14,7 @@ const navLink = document.querySelectorAll('.nav-link').forEach(el => {
     })  
 })
 
+// 3-4. click / dblclick
 const button = document.querySelectorAll('.btn').forEach(el => {
     el.addEventListener('mouseenter', () => {
         el.style.color = 'orange';
@@ -36,13 +39,45 @@ const button = document.querySelectorAll('.btn').forEach(el => {
     })  
 })
 
+// 5-7. mouseover, mousedown, mouseup
 const crazyBus = document.querySelector('.intro img');
 crazyBus.addEventListener('mouseover', e => 
         e.target.src = 'img/funny-bus-images02.jpg'
     )      
 crazyBus.addEventListener('mouseleave', e => 
     e.target.src = 'img/fun-bus.jpg'
-)
+    )
 crazyBus.addEventListener('mousedown', e => 
     e.target.src = 'img/lion_bus.jpg'    
-)      
+    )      
+crazyBus.addEventListener('mouseup', e =>
+    e.target.src = 'img/short_bus.jpg')    
+
+// const headers = document.querySelectorAll('h2').forEach( el => {
+//     el.addEventListener('scroll', () => {
+//     el.style.color= 'yellow';
+//   })
+// })
+
+// 8-10. contextmenu, resize, scroll
+const uglyBlue = document.querySelector('body');
+uglyBlue.addEventListener('contextmenu', () => {
+  uglyBlue.style.backgroundColor = 'blue';
+})
+
+const picsPoP = document.querySelectorAll('img').forEach(el => {
+    el.addEventListener('resize', () => {
+        el.style.transform = 'scale(1.2)';
+    })      
+})
+
+const resizeClown = document.querySelector('.intro img');
+window.addEventListener('resize', () => {
+    resizeClown.src = 'img/evil_clown.png';
+});
+
+const redact = document.querySelector('p');
+window.addEventListener('scroll', () => {
+    redact.style.backgroundColor = 'black';
+});
+
